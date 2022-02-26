6dcfd9832859e43569ac7f75625f21a4b944bde2
Adeolu Oluwasegun David <adeolusegun99@gmail.com>
2022-02-25 18:08:14 -0800

    text field repo

--- app/src/main/java/com/oluwasegun/displaytextview/MainActivity.kt
+++ app/src/main/java/com/oluwasegun/displaytextview/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.oluwasegun.displaytextview
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    var message: String = "Message: "
+    private lateinit var editTextMessage: EditText
+    private lateinit var btnSend: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,10 +22,17 @@ class MainActivity : AppCompatActivity() {
     override fun onResume(){
         super.onResume()
         val btnDisplay = findViewById<Button>(R.id.btnDisplay)
+        editTextMessage = findViewById(R.id.editText)
+
+        btnSend = findViewById(R.id.btnDisplay)
 
         btnDisplay.setOnClickListener {
+            var message = editTextMessage.text.toString().trim()
+
             val intent = Intent(this, TextField::class.java)
+            intent.putExtra("MY_DATA", message)
             startActivity(intent)
+            Log.i(message, message)
         }
 
     }
--- app/src/main/java/com/oluwasegun/displaytextview/TextField.kt
+++ app/src/main/java/com/oluwasegun/displaytextview/TextField.kt
@@ -1,11 +1,25 @@
 package com.oluwasegun.displaytextview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class TextField : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_field)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val data = intent.getStringExtra("MY_DATA")
+
+        textView = findViewById(R.id.textView)
+        textView.text = data
     }
 }
